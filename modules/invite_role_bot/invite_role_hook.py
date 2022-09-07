96884a3f448f4c13794a8c8b4c1313989180858d
wow13524 <50389483+wow13524@users.noreply.github.com>
2022-09-07 01:26:02 -0700

    Added missing guild join hook
    Added guild join hook for when bot is reinvited to a guild with existing invite-roles.  Also ensure that a guild has to have its invite uses up to date during startup before doing member join logic.

--- modules/invite_role_bot/invite_role_hook.py
+++ modules/invite_role_bot/invite_role_hook.py
@@ -1,6 +1,6 @@
 from discord import Forbidden,Guild,Invite,Member
 from modubot import ModuleBase
-from typing import List,Optional,TYPE_CHECKING
+from typing import List,Dict,Optional,TYPE_CHECKING
 
 if TYPE_CHECKING:
     from modubot import Bot
@@ -11,15 +11,21 @@ class Module(ModuleBase):
     def __init__(self,bot: 'Bot') -> None:
         self.bot: 'Bot' = bot
         self.persistence_layer: PersistenceLayer
+        self.ready_guilds: Dict[int,bool] = {}
     
     async def postinit(self) -> None:
         func_inject: FuncInject = self.bot.get_module("modules.core.func_inject")
         self.persistence_layer = self.bot.get_module("modules.invite_role_bot.persistence_layer")
 
+        func_inject.inject(self.on_guild_join)
         func_inject.inject(self.on_guild_update)
         func_inject.inject(self.on_member_join)
         func_inject.inject(self.on_ready)
     
+    async def on_guild_join(self,guild: Guild) -> None:
+        await self.persistence_layer.update_invite_uses(guild)
+        self.ready_guilds[guild.id] = True
+
     async def on_guild_update(self,before: Guild,after: Guild) -> None:
         try:
             before_vanity: Optional[Invite] = await before.vanity_invite()
@@ -31,11 +37,15 @@ class Module(ModuleBase):
             pass
 
     async def on_member_join(self,member: Member) -> None:
+        if member.guild.id not in self.ready_guilds:
+            return
         used_invites: List[Invite] = await self.persistence_layer.update_invite_uses(member.guild)
         assert len(used_invites) == 1, "Other invites used before join"
         used_invite: Invite = used_invites[0]
         await member.add_roles(*await self.persistence_layer.get_invite_roles(used_invite),reason=f"Invite-roles for {used_invite.code}")
 
     async def on_ready(self) -> None:
+        self.ready_guilds = {}
         for guild in self.bot.guilds:
             await self.persistence_layer.update_invite_uses(guild)
+            self.ready_guilds[guild.id] = True
