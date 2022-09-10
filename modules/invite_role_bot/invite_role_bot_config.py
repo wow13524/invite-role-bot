0e0daf6eefb9ac25e25b9720e0322654c4561224
wow13524 <50389483+wow13524@users.noreply.github.com>
2022-09-10 03:40:05 -0700

    Shortened quickstart config.json

--- modules/invite_role_bot/invite_role_bot_config.py
+++ modules/invite_role_bot/invite_role_bot_config.py
@@ -1,10 +1,10 @@
 from modubot import PropertyDict
 
 class HelpConfig(PropertyDict):
-    invite_url: str = ""
-    github_url: str = ""
-    privacy_policy_url: str = ""
-    donate_url: str = ""
+    invite_url: str = "https://top.gg/bot/480240313525600267"
+    github_url: str = "https://github.com/wow13524/invite-role-bot"
+    privacy_policy_url: str = "https://gist.github.com/wow13524/63054dd96bd4641104d0924e85f0501e"
+    donate_url: str = "https://www.paypal.me/wow13524"
 
 class InviteRoleBotConfig(PropertyDict):
     help_command: HelpConfig = HelpConfig([],{},"")
