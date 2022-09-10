bbd958b35636d16ac5d7ff63342b2b6b6d9dbeea
wow13524 <50389483+wow13524@users.noreply.github.com>
2022-09-10 03:29:22 -0700

    Added unavailable guild handling

---
+++ README.md
@@ -0,0 +1,25 @@
+# Invite-Role Bot
+
+A bot that assigns roles to new guild members depending on the invite URL used to join, written in Python.
+
+---
+
+## Commands
+> ### `/help`
+> Shows a help menu containing the command list and some useful links.
+> 
+> ### `/invrole connect [invite_url: str] [role: Role]`
+> Connects any invite URL from the guild to any role.  One invite URL can have multiple roles connected.
+> 
+> ### `/invrole disconnect [invite_url: str] <role: Role>`
+> Disconnects one or all roles from the specified invite URL.  If a role is given, only that role is disconnected from the invite URL; otherwise, every role associated with the given invite URL will be disconnected.
+> 
+> `/invrole list`
+> Shows a list of all invite-role connections within this guild.
+
+## Getting Started
+*Note: The bot application used should have the **Server Members Intent** enabled through the Discord developers dashboard.*
+
+Clone the repository, populate `BotConfig.token` in the provided `config.json`, and run `main.py`.
+
+The bot should connect to Discord and function with no further configuration needed.
--- modules/invite_role_bot/invite_role_hook.py
+++ modules/invite_role_bot/invite_role_hook.py
@@ -18,12 +18,19 @@ class Module(ModuleBase):
         func_inject: FuncInject = self.bot.get_module("modules.core.func_inject")
         self.persistence_layer = self.bot.get_module("modules.invite_role_bot.persistence_layer")
 
+        func_inject.inject(self.on_guild_available)
         func_inject.inject(self.on_guild_join)
         func_inject.inject(self.on_guild_update)
         func_inject.inject(self.on_guild_role_update)
+        func_inject.inject(self.on_guild_unavailable)
         func_inject.inject(self.on_member_join)
         func_inject.inject(self.on_ready)
-    
+
+    async def on_guild_available(self,guild: Guild) -> None:
+        if guild.id in self.ready_guilds:
+            await self.persistence_layer.update_invite_uses(guild)
+            self.ready_guilds[guild.id] = True
+
     async def on_guild_join(self,guild: Guild) -> None:
         await self.persistence_layer.update_invite_uses(guild)
         self.ready_guilds[guild.id] = True
@@ -41,8 +48,12 @@ class Module(ModuleBase):
     async def on_guild_role_update(self,before: Role,after: Role) -> None:
             await self.persistence_layer.update_invite_uses(after.guild)
 
+    async def on_guild_unavailable(self,guild: Guild) -> None:
+        if guild.id in self.ready_guilds:
+            self.ready_guilds[guild.id] = False
+
     async def on_member_join(self,member: Member) -> None:
-        if member.guild.id not in self.ready_guilds or not member.guild.me.guild_permissions.manage_guild or not member.guild.me.guild_permissions.manage_roles:
+        if member.guild.id not in self.ready_guilds or not self.ready_guilds[member.guild.id] or not member.guild.me.guild_permissions.manage_guild or not member.guild.me.guild_permissions.manage_roles:
             return
         used_invites: List[Invite] = await self.persistence_layer.update_invite_uses(member.guild)
         if used_invites:
@@ -54,6 +65,7 @@ class Module(ModuleBase):
         self.ready_guilds = {}
         await self.bot.change_presence(status=Status.idle,activity=Game(name="Starting up..."))
         for guild in tqdm(self.bot.guilds,desc="Updating Cached Guild Invites",unit="guilds"):
-            await self.persistence_layer.update_invite_uses(guild)
+            if not guild.unavailable:
+                await self.persistence_layer.update_invite_uses(guild)
             self.ready_guilds[guild.id] = True
         await self.bot.change_presence(status=Status.online,activity=Game(name="'/help' for help!"))
