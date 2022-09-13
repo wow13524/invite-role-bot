c6897fdef3fbfe4a41fac2aba0027b2b42afd2b0
wow13524 <50389483+wow13524@users.noreply.github.com>
2022-09-13 16:26:20 -0700

    Speed and stability optimization
    Startup caching is now "parallelized" between shards so caching time should remain constant independent of guild count, with each shard managing ~1k guilds.  Invites are recached whenever a shard reconnects and presences are updated accordingly in the event of an internet outage.

--- modules/invite_role_bot/invite_role_hook.py
+++ modules/invite_role_bot/invite_role_hook.py
@@ -25,8 +25,7 @@ class Module(ModuleBase):
         func_inject.inject(self.on_guild_role_update)
         func_inject.inject(self.on_guild_unavailable)
         func_inject.inject(self.on_member_join)
-        func_inject.inject(self.on_ready)
-        func_inject.inject(self.on_shard_resumed)
+        func_inject.inject(self.on_shard_connect)
 
     async def on_guild_available(self,guild: Guild) -> None:
         if guild.id in self.ready_guilds and not self.ready_guilds[guild.id]:
@@ -67,21 +66,19 @@ class Module(ModuleBase):
             active_roles: List[Role]
             active_roles,_ = await self.persistence_layer.get_invite_roles(member.guild,used_invite.code)
             await member.add_roles(*active_roles,reason=f"Invite-roles for {used_invite.code}")
-
-    async def on_ready(self) -> None:
-        self.ready_guilds = {}
-        await self.bot.change_presence(status=Status.idle,activity=Game(name="Starting up..."))
-        guilds_map: Dict[int,Guild] = {guild.id:guild for guild in self.bot.guilds}
-        for guild_id in tqdm(await self.persistence_layer.get_guild_ids(),desc="Updating Cached Guild Invites",unit="guilds"):
-            guild: Optional[Guild] = guilds_map[guild_id] if guild_id in guilds_map else None
-            if guild and not guild.unavailable:
-                while guild.id not in self.ready_guilds:
+    
+    async def on_shard_connect(self,shard_id: int) -> None:
+        await self.bot.wait_until_ready()
+        await self.bot.change_presence(status=Status.idle,activity=Game(name="Starting up..."),shard_id=shard_id)
+        shard_guild_map: Dict[int,Guild] = {guild.id:guild for guild in self.bot.guilds if guild.shard_id == shard_id}
+        for guild_id in shard_guild_map.keys():
+            self.ready_guilds[guild_id] = False
+        for guild_id,guild in tqdm(shard_guild_map.items(),desc=f"Caching Invites for Shard #{shard_id}",unit="guilds"):
+            if not guild.unavailable:
+                while not self.ready_guilds[guild_id]:
                     try:
                         await self.persistence_layer.update_invite_uses(guild)
                         self.ready_guilds[guild.id] = True
                     except ServerDisconnectedError:
-                        await self.bot.wait_for("connect")
-        await self.bot.change_presence(status=Status.online,activity=Game(name="'/help' for help!"))
-    
-    async def on_shard_resumed(self,shard_id: int) -> None:
+                        return
         await self.bot.change_presence(status=Status.online,activity=Game(name="'/help' for help!"),shard_id=shard_id)
