ab8beb0698f0a681ac948072a059f26a3fe7ae01
wow13524 <50389483+wow13524@users.noreply.github.com>
2022-09-19 17:54:14 -0700

    Fixed role hierarchy response body size error
    If a guild has over 200 roles, the role hierarchy response will fail to send due to a description size of over 4096.  The description is now truncated to show only the most necessary roles.

--- modules/invite_role_bot/responses/role_hierarchy_response.py
+++ modules/invite_role_bot/responses/role_hierarchy_response.py
@@ -1,22 +1,33 @@
 from discord import Embed,Interaction,Role
 from . import error_response
 
-MAX_SPACING: int = 26
+MAX_SPACING: int = 32
+ROLE_OVERFLOW: int = 2
 
 def embed(interaction: Interaction,description: str,role_a: Role,label_a: str,role_b: Role,label_b: str) -> Embed:
     assert interaction.guild
     hierarchy: str = ""
     spacing_amount: int = min(MAX_SPACING,max(len(role.name)+1 for role in interaction.guild.roles))
-    for role in interaction.guild.roles[::-1]:
-        spacing: str = role.name.ljust(spacing_amount,' ').split(role.name)[1]
-        hierarchy += f"\n{role.mention}{spacing}"
-        if role == role_a or role == role_b:
-            hierarchy += "     ⬅️     **"
-            if role_a == role_b:
-                hierarchy += f"{label_a} & {label_b}"
-            elif role == role_a:
-                hierarchy += label_a
-            else:
-                hierarchy += label_b
-            hierarchy += "**"
+    index_a: int = interaction.guild.roles.index(role_a)
+    index_b: int = interaction.guild.roles.index(role_b)
+    gap_state_curr: int = -1
+    gap_state_next: int = 0
+    for i,role in enumerate(interaction.guild.roles):
+        if i in range(index_a-ROLE_OVERFLOW,index_a+ROLE_OVERFLOW+1) or i in range(index_b-ROLE_OVERFLOW,index_b+ROLE_OVERFLOW+1):
+            spacing: str = role.name.ljust(spacing_amount,' ').split(role.name)[1]
+            line: str = f"{role.mention}{spacing}"
+            if role == role_a or role == role_b:
+                gap_state_next += 1
+                line += "     ⬅️     **"
+                if role_a == role_b:
+                    line += f"{label_a}/{label_b}"
+                elif role == role_a:
+                    line += label_a
+                else:
+                    line += label_b
+                line += "**"
+            hierarchy = f"\n{line}"+hierarchy
+        elif gap_state_curr != gap_state_next:
+            gap_state_curr = gap_state_next
+            hierarchy = f"\n**.   .   .   .   .**"+hierarchy
     return error_response.embed(interaction,f"{description}\n{hierarchy}")
