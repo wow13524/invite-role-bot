c07650e4c4155894fbcb4c5af4b89028f50b9906
wow13524 <50389483+wow13524@users.noreply.github.com>
2022-10-11 23:39:36 -0700

    Use new cursor method

--- modules/invite_role_bot/persistence_layer.py
+++ modules/invite_role_bot/persistence_layer.py
@@ -1,5 +1,5 @@
 from sqlite3 import Row
-from aiosqlite import Connection,Cursor
+from aiosqlite import Cursor
 from cachetools import TTLCache
 from discord import Guild,Invite,Object,PartialInviteChannel,PartialInviteGuild,Role
 from discord.abc import GuildChannel
@@ -18,13 +18,12 @@ class Module(ModuleBase):
     def __init__(self,bot: 'Bot') -> None:
         self._bot: Bot = bot
         self._cached_invites: TTLCache[int,List[Invite]] = TTLCache(maxsize=512,ttl=30)
-        self._connection: Connection
+        self._db_sqlite: DBSQLite
     
     async def postinit(self) -> None:
-        db_sqlite: DBSQLite = self._bot.get_module("modules.persistence.db_sqlite")
-        self._connection = db_sqlite.connection
+        self._db_sqlite: DBSQLite = self._bot.get_module("modules.persistence.db_sqlite")
 
-        cur: Cursor = await self._connection.cursor()
+        cur: Cursor = await self._db_sqlite.cursor()
         await cur.execute(
             """
             CREATE TABLE IF NOT EXISTS guilds (
@@ -53,58 +52,58 @@ class Module(ModuleBase):
         )
     
     async def _raw_guild_exists(self,guild_id: int) -> bool:
-        cur: Cursor = await self._connection.cursor()
+        cur: Cursor = await self._db_sqlite.cursor()
         return await (await cur.execute("SELECT 1 FROM guilds WHERE id = ?;",[guild_id])).fetchone() is not None
 
     async def _raw_invite_exists(self,invite_code: str) -> bool:
-        cur: Cursor = await self._connection.cursor()
+        cur: Cursor = await self._db_sqlite.cursor()
         return await (await cur.execute("SELECT 1 FROM invites WHERE code = ?;",[invite_code])).fetchone() is not None
 
     async def _raw_invite_role_exists(self,invite_code: str,role_id: Optional[int]) -> bool:
-        cur: Cursor = await self._connection.cursor()
+        cur: Cursor = await self._db_sqlite.cursor()
         if role_id:
             return await (await cur.execute("SELECT 1 FROM roles WHERE id = ? AND invite_code = ?;",[role_id,invite_code])).fetchone() is not None
         else:
             return await self._raw_invite_exists(invite_code)
     
     async def _raw_add_guild(self,guild_id: int) -> None:
-        cur: Cursor = await self._connection.cursor()
+        cur: Cursor = await self._db_sqlite.cursor()
         if not await self._raw_guild_exists(guild_id):
             await cur.execute("INSERT INTO guilds (id) VALUES (?);",[guild_id])
     
     async def _raw_remove_guild_if_unused(self,guild_id: int) -> None:
-        cur: Cursor = await self._connection.cursor()
+        cur: Cursor = await self._db_sqlite.cursor()
         if not await (await cur.execute("SELECT 1 FROM invites WHERE guild_id = ?;",[guild_id])).fetchone():
             await cur.execute("DELETE FROM guilds WHERE id = ?;",[guild_id])
     
     async def _raw_add_invite(self,guild_id: int,invite_code: str,invite_uses: int) -> None:
         await self._raw_add_guild(guild_id)
-        cur: Cursor = await self._connection.cursor()
+        cur: Cursor = await self._db_sqlite.cursor()
         if not await self._raw_invite_exists(invite_code):
             await cur.execute("INSERT INTO invites (code,uses,guild_id) VALUES (?,?,?);",[invite_code,invite_uses,guild_id])
     
     async def _raw_remove_invite_if_unused(self,guild_id: int,invite_code: str) -> None:
-        cur: Cursor = await self._connection.cursor()
+        cur: Cursor = await self._db_sqlite.cursor()
         if not await (await cur.execute("SELECT 1 FROM roles WHERE invite_code = ?;",[invite_code])).fetchone():
             await cur.execute("DELETE FROM invites WHERE code = ?;",[invite_code])
             await self._raw_remove_guild_if_unused(guild_id)
 
     async def _raw_get_invite_codes(self,guild_id: int) -> List[str]:
-        cur: Cursor = await self._connection.cursor()
+        cur: Cursor = await self._db_sqlite.cursor()
         return [x[0] for x in await (await cur.execute("SELECT code FROM invites WHERE guild_id = ?;",[guild_id])).fetchall()]
     
     async def _raw_get_invite_role_ids(self,invite_code: str) -> List[int]:
-        cur: Cursor = await self._connection.cursor()
+        cur: Cursor = await self._db_sqlite.cursor()
         return [x[0] for x in await (await cur.execute("SELECT id FROM roles WHERE invite_code = ?;",[invite_code])).fetchall()]
     
     async def _raw_add_invite_role(self,guild_id: int,invite_code: str,invite_uses: int,role_id: int) -> None:
-        cur: Cursor = await self._connection.cursor()
+        cur: Cursor = await self._db_sqlite.cursor()
         if not await self._raw_invite_role_exists(invite_code,role_id):
             await self._raw_add_invite(guild_id,invite_code,invite_uses)
             await cur.execute("INSERT INTO roles (id,invite_code) VALUES (?,?);",[role_id,invite_code])
     
     async def _raw_remove_invite_role(self,guild_id: int,invite_code: str,role_id: Optional[int]) -> None:
-        cur: Cursor = await self._connection.cursor()
+        cur: Cursor = await self._db_sqlite.cursor()
         if role_id:
             await cur.execute("DELETE FROM roles WHERE invite_code = ? AND id = ?;",[invite_code,role_id])
         else:
@@ -118,7 +117,7 @@ class Module(ModuleBase):
         return await self._raw_invite_role_exists(invite_code,role.id if role else None)
 
     async def get_guild_ids(self) -> List[int]:
-        cur: Cursor = await self._connection.cursor()
+        cur: Cursor = await self._db_sqlite.cursor()
         return [int(x[0]) for x in await (await cur.execute("SELECT id FROM guilds;")).fetchall()]
 
     async def get_invite_codes(self,guild: Guild) -> List[str]:
@@ -193,7 +192,7 @@ class Module(ModuleBase):
     
     async def update_invite_uses(self,guild: Guild) -> List[Invite]:
         used_invites: List[Invite] = []
-        cur: Cursor = await self._connection.cursor()
+        cur: Cursor = await self._db_sqlite.cursor()
         if guild.me.guild_permissions.manage_guild:
             for invite in await self.get_invites(guild,False):
                 saved_uses_row: Optional[Row] = await (await cur.execute("SELECT uses FROM invites WHERE code = ?;",[invite.code])).fetchone()
