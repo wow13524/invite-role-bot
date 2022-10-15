048b4b51830d2ffcbf6956d6bd7662501a9f90b8
wow13524 <50389483+wow13524@users.noreply.github.com>
2022-10-15 00:03:48 -0700

    Pull latest Modubot

--- modubot/bot.py
+++ modubot/bot.py
@@ -32,12 +32,33 @@ class Bot(discord.AutoShardedClient):
         self._config: Config = Config(config_path=os.path.join(work_dir,config_name))
         self._bot_config: BotConfig = self._config.get(BotConfig)
         self._loaded_modules: Dict[str,ModuleBase] = self._preload_modules()
-
+        
+        assert "intents" not in self._bot_config.client_args, "intents must be defined in BotConfig.intents, not BotConfig.client_args"
+        parsed_client_args: Dict[str,Any] = {}
+        for option,raw_value in self._bot_config.client_args.items():
+            if type(raw_value) == dict and "_class" in raw_value:
+                raw_value: Dict[str,Any] = raw_value
+                assert hasattr(discord,raw_value["_class"]), f"invalid class '{raw_value['_class']}' present in config"
+                value_class: type = getattr(discord,raw_value["_class"])
+                option_value: Any
+                if "_attr" in raw_value:
+                    assert hasattr(value_class,raw_value["_attr"]), f"invalid attribute '{raw_value['_class']}.{raw_value['_attr']}' present in config"
+                    option_value = getattr(value_class,raw_value["_attr"])
+                    assert raw_value["_class"] in str(type(option_value)), f"attribute '{raw_value['_class']}.{raw_value['_attr']}' present in config is not of type {raw_value['class']}"
+                else:
+                    option_value = value_class() if not hasattr(value_class,"none") else getattr(value_class,"none")()
+                    for k,v in raw_value.items():
+                        if not k.startswith("_"):
+                            setattr(option_value,k,v)
+                parsed_client_args[option] = option_value
+                continue
+            parsed_client_args[option] = raw_value
+        
         intents: discord.Intents = discord.Intents.none()
         for intent,value in self._bot_config.intents.items():
             assert hasattr(intents,intent), f"invalid intent '{intent}' present in config"
             setattr(intents,intent,bool(value))
-        super().__init__(intents=intents,**self._bot_config.client_args)
+        super().__init__(intents=intents,**parsed_client_args)
     
     def _preload_modules(self) -> Dict[str,ModuleBase]:
         loaded_modules: Dict[str,ModuleBase] = {}
